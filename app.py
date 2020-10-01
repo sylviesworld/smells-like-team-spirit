c217c3a4cd003e482d77712ebf7b499dea0129f0
Dylan Lomax <dlomax@vols.utk.edu>
2020-09-30 22:19:23 -0400

    Significant work for #28
    I have made significant progress on integrating this new layout with the main app. I'm having trouble correctly linking the buttons though.

--- app.py
+++ app.py
@@ -6,16 +6,22 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtPrintSupport import QPrintDialog, QPrinter
 from app_widget import AppWidget
+from find_window import FindWindow
 
-class MainWindow(QtWidgets.QMainWindow):
+qtcreator_file = "app.ui"
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtcreator_file)
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent)
-        uic.loadUi("app.ui", self)
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
 
          # Get screen resolution
         screenBounds = QDesktopWidget().screenGeometry(0)
 
-        self.setWindowTitle('Notepad App - untitled.txt')
+        self.window_title = 'Notepad App - untitled.txt'
+        self.setWindowTitle(self.window_title)
         self.setGeometry(0, 0, round(screenBounds.width()/2), round(screenBounds.height()/2))
 
         # Center the screen
@@ -24,77 +30,289 @@ class MainWindow(QtWidgets.QMainWindow):
         rect.moveCenter(centerScreen)
         self.move(rect.topLeft())
 
-        # Set central widget for window
-        self.centralWidget = AppWidget()
-        self.setCentralWidget(self.centralWidget)
-        self.centralWidget.textBox.textChanged.connect(self.textEditedEvent)
+        self.setCentralWidget(self.centralwidget)
+        self.verticalLayout.addWidget(self.textEdit)
+        self.textEdit.textChanged.connect(self.textEditedEvent)
+        self.findWindow = FindWindow(self.textEdit)
+
+        self.commandLinkButton.clicked.connect(lambda: self.openNoteSlot(True))
+        self.commandLinkButton_2.clicked.connect(lambda: self.openNoteSlot(False))
+        self.commandLinkButton_3.clicked.connect(self.saveNoteSlot)
+        self.commandLinkButton_4.clicked.connect(self.saveAsNoteSlot)
+
+        # Define status bar
+        self.status = QStatusBar()
+        self.setStatusBar(self.status)
         self.needsSave = False
+        self.currentFile = ''
+
+        menuBar = self.menuBar
+        self.setMenuBar(menuBar)
+
+        # Define status bar
+        self.setStatusBar(self.statusbar)
+        self.setStatusBar(self.status)
+
+        # Create menu bars
+        fileMenu = menuBar.addMenu('File')
+
+        formatMenu = menuBar.addMenu('Format')
+
+        # File menu bar
+        newButton = QAction('New', self)
+        newButton.setShortcut('Ctrl+N')
+        newButton.triggered.connect(lambda: self.openNoteSlot(True))
+        fileMenu.addAction(newButton)
+
+        openButton = QAction('Open...', self)
+        openButton.setShortcut('Ctrl+O')
+        openButton.triggered.connect(lambda: self.openNoteSlot(False))
+        fileMenu.addAction(openButton)
+
+        closeButton = QAction('Close', self)
+        closeButton.setShortcut('Ctrl+W')
+        closeButton.triggered.connect(lambda: self.closeEvent)
+        fileMenu.addAction(closeButton)
+
+        fileMenu.addSeparator()
+
+        saveIcon = QIcon.fromTheme('document-save')
+        saveButton = QAction(saveIcon, 'Save', self)
+        saveButton.setShortcut('Ctrl+S')
+        saveButton.triggered.connect(self.saveNoteSlot)
+        self.currentFile = ''
+        fileMenu.addAction(saveButton)
+
+        saveAsButton = QAction(saveIcon, 'Save As...', self)
+        saveAsButton.setShortcut('Ctrl+Shift+S')
+        saveAsButton.triggered.connect(self.saveAsNoteSlot)
+        fileMenu.addAction(saveAsButton)
+
+        printButton = QAction(saveIcon, 'Print', self)
+        printButton.setShortcut('Ctrl+P')
+        printButton.triggered.connect(self.printEvent)
+        fileMenu.addAction(printButton)
+
+        # Edit menu bar
+        editMenu = menuBar.addMenu('Edit')
+
+        undoButton = QAction('Undo', self)
+        undoButton.setShortcut(QKeySequence.Undo)
+        undoButton.triggered.connect(self.textEdit.undo)
+        editMenu.addAction(undoButton)
+
+        redoButton = QAction('Redo', self)
+        redoButton.setShortcut(QKeySequence.Redo)
+        redoButton.triggered.connect(self.textEdit.redo)
+        editMenu.addAction(redoButton)
+
+        editMenu.addSeparator()
+
+        cutButton = QAction('Cut', self)
+        cutButton.setShortcut(QKeySequence.Cut)
+        cutButton.triggered.connect(self.textEdit.cut)
+        editMenu.addAction(cutButton)
+
+        copyButton = QAction('Copy', self)
+        copyButton.setStatusTip('Copy Selected Text to Clipboard')
+        copyButton.setShortcut(QKeySequence.Copy)
+        copyButton.triggered.connect(self.textEdit.copy)
+        editMenu.addAction(copyButton)
+
+        # Duplicate paste edit menu button
+        # pasteButton = QAction('Paste', self)
+        # pasteButton.setShortcut(QKeySequence.Paste)
+        # pasteButton.triggered.connect(self.textEdit.paste)
+        # editMenu.addAction(pasteButton)
+
+        # Format menu button
+        # fontButton = QAction('Font', self)
+        # fontButton.triggered.connect(self.fontChoice)
+        # formatMenu.addAction(fontButton)
+
+        # Create toolbars
+        # file_toolbar = QToolBar("File")
+        # file_toolbar.setIconSize(QSize(18, 18))
+        # self.addToolBar(file_toolbar)
+
+        paste_action = QAction('Paste', self)
+        paste_action.setShortcut(QKeySequence.Paste)
+        paste_action.setStatusTip('Paste From Clipboard')
+        paste_action.triggered.connect(self.textEdit.paste)
+        editMenu.addAction(paste_action)
+
+        selectButton = QAction('Select All', self)
+        selectButton.setShortcut(QKeySequence.SelectAll)
+        selectButton.triggered.connect(self.textEdit.selectAll)
+        editMenu.addAction(selectButton)
+
+        findButton = QAction('Find', self)
+        findButton.setShortcut(QKeySequence.Find)
+        findButton.triggered.connect(
+            self.findWindow.createWindow)
+        editMenu.addAction(findButton)
+
+        # Format menu button
+        fontButton = QAction('Font (System Dialogue)', self)
+        fontButton.triggered.connect(self.fontChoice)
+        formatMenu.addAction(fontButton)
+
+        # Create edit toolbar
+        edit_toolbar = QToolBar("Edit")
+        edit_toolbar.setIconSize(QSize(18, 18))
+        self.addToolBar(edit_toolbar)
+
+        cut_action = QAction(
+            QIcon(os.path.join('images', 'icons8-cut-80.png')), 'Cut', self)
+        cut_action.setStatusTip('Cut Selected Text (Copy and Delete)')
+        cut_action.setShortcut(QKeySequence.Cut)
+        cut_action.triggered.connect(self.textEdit.cut)
+        edit_toolbar.addAction(cut_action)
+
+        copy_action = QAction(
+            QIcon(os.path.join('images', 'icons8-copy-80.png')), 'Copy', self)
+        copy_action.setStatusTip('Copy Selected Text to Clipboard')
+        copy_action.setShortcut(QKeySequence.Copy)
+        copy_action.triggered.connect(self.textEdit.copy)
+        edit_toolbar.addAction(copy_action)
+
+        paste_action = QAction(
+            QIcon(os.path.join('images', 'icons8-paste-80.png')), 'Paste', self)
+        paste_action.setStatusTip('Paste From Clipboard')
+        paste_action.setShortcut(QKeySequence.Paste)
+        paste_action.triggered.connect(self.textEdit.paste)
+        edit_toolbar.addAction(paste_action)
+
+        # Create font toolbar
+        font_toolbar = QToolBar("Font")
+        font_toolbar.setIconSize(QSize(18, 18))
+        self.addToolBar(font_toolbar)
+
+        # font_choice_action = QAction('Font', self)
+        # font_choice_action.setStatusTip(
+        #     'Open System Dialogue for Font Choice')
+        # font_choice_action.triggered.connect(self.fontChoice)
+        # font_toolbar.addAction(font_choice_action)
+
+        self.fonts = QFontComboBox()
+        self.fonts.currentFontChanged.connect(
+            self.textEdit.setCurrentFont)
+        font_toolbar.addWidget(self.fonts)
+
+        FONT_SIZES = [5, 5.5, 6.5, 7.5, 8, 9, 10, 10.5, 11]
+        FONT_SIZES.extend(range(12, 30, 2))
+        FONT_SIZES.extend([36, 48, 72])
+
+        self.fontsize = QComboBox()
+        self.fontsize.addItems([str(s) for s in FONT_SIZES])
+        self.fontsize.currentIndexChanged[str].connect(
+            lambda s: self.textEdit.setFontPointSize(float(s)))
+        font_toolbar.addWidget(self.fontsize)
+
+        # Create format toolbar
+        format_toolbar = QToolBar("Format")
+        format_toolbar.setIconSize(QSize(18, 18))
+        self.addToolBar(format_toolbar)
+
+        bold_action = QAction(
+            QIcon(os.path.join('images', 'icons8-bold-80.png')), "Bold", self)
+        bold_action.setStatusTip("Set selected text to Bold (strong)")
+        bold_action.setShortcut(QKeySequence.Bold)
+        bold_action.setCheckable(True)
+        bold_action.toggled.connect(lambda x: self.textEdit.setFontWeight(
+            QFont.Bold if x else QFont.Normal))
+        format_toolbar.addAction(bold_action)
+        formatMenu.addAction(bold_action)
+
+        italic_action = QAction(
+            QIcon(os.path.join('images', 'icons8-italic-80.png')), "Italic", self)
+        italic_action.setStatusTip("Set selected text to Italic (emphasis)")
+        italic_action.setShortcut(QKeySequence.Italic)
+        italic_action.setCheckable(True)
+        italic_action.toggled.connect(self.textEdit.setFontItalic)
+        format_toolbar.addAction(italic_action)
+        formatMenu.addAction(italic_action)
+
+        underline_action = QAction(
+            QIcon(os.path.join('images', 'icons8-underline-80.png')), "Underline", self)
+        underline_action.setStatusTip("Set selected text to Underline")
+        underline_action.setShortcut(QKeySequence.Underline)
+        underline_action.setCheckable(True)
+        underline_action.toggled.connect(
+            self.textEdit.setFontUnderline)
+        format_toolbar.addAction(underline_action)
+        formatMenu.addAction(underline_action)
+
+        self.currentColor = Qt.black
+        self.textEdit.setTextColor(
+            Qt.black)  # Set inital text color HTML
+        colorAction = QAction(
+            QIcon(os.path.join('images', 'icons8-text-color-80.png')), 'Color', self)
+        colorAction.triggered.connect(self.colorPicker)
+        format_toolbar.addAction(colorAction)
+        formatMenu.addAction(colorAction)
+
+        font = QFont('Helvetica', 16)
+        self.textEdit.setFont(font)
+        self.textEdit.setFontPointSize(16)
+
+        # Create paragraph toolbar
+        paragraph_toolbar = QToolBar("Paragraph")
+        paragraph_toolbar.setIconSize(QSize(18, 18))
+        self.addToolBar(paragraph_toolbar)
+
+        self.aln_left_action = QAction(
+            QIcon(os.path.join('images', 'icons8-align-left-80.png')), "Align Left", self)
+        self.aln_left_action.setStatusTip("Align Text Left")
+        self.aln_left_action.setChecked(True)
+        self.aln_left_action.triggered.connect(
+            lambda: self.textEdit.setAlignment(Qt.AlignLeft))
+        paragraph_toolbar.addAction(self.aln_left_action)
+
+        self.aln_center_action = QAction(
+            QIcon(os.path.join('images', 'icons8-align-center-80.png')), "Center", self)
+        self.aln_center_action.setStatusTip("Center Text")
+        self.aln_center_action.setChecked(True)
+        self.aln_center_action.triggered.connect(
+            lambda: self.textEdit.setAlignment(Qt.AlignCenter))
+        paragraph_toolbar.addAction(self.aln_center_action)
+
+        self.aln_right_action = QAction(QIcon(os.path.join(
+            'images', 'icons8-align-right-80.png')), "Align Right", self)
+        self.aln_right_action.setStatusTip("Align Text Right")
+        self.aln_right_action.setChecked(True)
+        self.aln_right_action.triggered.connect(
+            lambda: self.textEdit.setAlignment(Qt.AlignRight))
+        paragraph_toolbar.addAction(self.aln_right_action)
+
+        self.aln_justify_action = QAction(
+            QIcon(os.path.join('images', 'icons8-align-justify-80.png')), "Justify", self)
+        self.aln_justify_action.setStatusTip("Justify Text")
+        self.aln_justify_action.setChecked(True)
+        self.aln_justify_action.triggered.connect(
+            lambda: self.textEdit.setAlignment(Qt.AlignJustify))
+        paragraph_toolbar.addAction(self.aln_justify_action)
 
     def fontChoice(self):
         font, valid = QFontDialog.getFont()
         if valid:
             # self.styleChoice.setFont(font)
-            self.setFont(font)
+            # self.setFont(font)
+            self.textEdit.setFont(font)
 
-    # Called when the QMainWindow is closed
-    def closeEvent(self, event):
-        if self.needsSave:
-            self.promptSaveMessage()
+    # Opens the color dialog
+    def colorPicker(self):
+        cursor = self.textEdit.textCursor()
+        color = QColorDialog.getColor(self.currentColor)
 
-    # Called when any key is pressed
-    def keyPressEvent(self, e):
-        self.statusBar().clearMessage()
+        if color.isValid():
+            if cursor.hasSelection():
+                cursor.insertHtml('<font color= "' + color.name() +
+                                  '">' + cursor.selectedText() + '</font>')
+            else:
+                self.textEdit.setTextColor(color)
 
-    # Called when text in the AppWidget QTextEdit is changed
-    def textEditedEvent(self):
-        self.needsSave = True
-        self.statusBar().clearMessage()
-
-
-          # Opens the file dialog to save a new file or the working file. Returns false if canceled
-    def saveEvent(self):
-        if self.currentFile == '':
-            fileName, _ = QFileDialog.getSaveFileName(
-                self, 'Save As', '', 'Text Files (*.txt);;PDF Files (*.pdf)')
-
-            path, extension = os.path.splitext(fileName)
-
-            # Save text file
-            if fileName and extension == '.txt':
-                self.centralWidget.saveFile(fileName)
-                self.currentFile = fileName
-                self.statusBar().showMessage('File saved.')
-                self.needsSave = False
-                self.setWindowTitle('Notepad App - ' +
-                                    os.path.basename(fileName))
-                return True
-            
-            # Save PDF
-            elif fileName and extension == '.pdf' :
-                self.savePDF(fileName)
-                return True
-
-            # File dialog canceled
-            return False
-
-        else:
-            self.centralWidget.saveFile(self.currentFile)
-            self.statusBar().showMessage('File saved.')
-            self.needsSave = False
-            return True
-
-    # Opens the file dialog even if a file is already open. Returns false if canceled
-    def saveAsEvent(self):
-
-        oldFile = self.currentFile
-        self.currentFile = ''
-
-        # File dialog canceled, reset to original file name
-        if self.saveEvent():
-            self.currentFile = oldFile
-            return False
-
-        return True
+            self.currentColor = color
 
     # Opens a file (isNew defines if the file is a new, empty file)
     def openNoteSlot(self, isNew: bool):
@@ -110,7 +328,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
             # New file
             if isNew:
-                self.centralWidget.textBox.clear()
+                self.textEdit.textBox.clear()
                 self.setWindowTitle('Notepad App - untitled.txt')
 
             # Open file dialog
@@ -119,12 +337,45 @@ class MainWindow(QtWidgets.QMainWindow):
                     self, 'Open File', '', 'Text Files (*.txt *.pdf)')
 
                 if fileName:
-                    self.centralWidget.openFile(fileName)
+                    f = open(fileName, 'r')
+                    self.textBox.setPlainText(f.read())
+                    f.close()
+                    #self.textEdit.openFile(fileName)
                     self.currentFile = fileName
                     self.setWindowTitle(
                         'Notepad App - ' + os.path.basename(fileName) + ' -- Last Modified - ' + time.ctime(os.path.getmtime(fileName)))
 
             self.needsSave = False
+
+    # Called when the QMainWindow is closed
+    def closeEvent(self, event):
+        if self.needsSave:
+            reply = QMessageBox.question(self, 'Window Close', 'Do you want to save changes to the current file?', 
+                        QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel)
+            
+            if reply == QMessageBox.Yes:
+                self.saveMessageSuccess = self.saveEvent()
+                event.accept()
+
+            elif reply == QMessageBox.No:
+                event.accept()
+
+            else:
+                event.ignore()
+
+    # Called when text in the AppWidget QTextEdit is changed
+    def textEditedEvent(self):
+        self.needsSave = True
+        self.statusBar().clearMessage()
+        if "Edited" not in str(self.window_title):
+            self.window_title = self.window_title + " -- Edited"
+            self.setWindowTitle(self.window_title)
+
+     # Opens file and reads the text to QTextEdit
+    def openFile(self, fileName):
+        f = open(fileName, 'r')
+        self.textBox.setPlainText(f.read())
+        f.close()
 
     # Creates the save message prompt window
     def promptSaveMessage(self):
@@ -165,23 +416,50 @@ class MainWindow(QtWidgets.QMainWindow):
         self.centralWidget.textBox.document().print_(printer)
 
 
-    @QtCore.pyqtSlot()
-    def newNoteSlot():
-        pass
+    def saveNoteSlot(self):
+        if self.currentFile == '':
+            fileName, _ = QFileDialog.getSaveFileName(
+                self, 'Save As', '', 'Text Files (*.txt);;PDF Files (*.pdf)')
 
-    @QtCore.pyqtSlot()
-    def saveNoteSlot():
-        pass
+            path, extension = os.path.splitext(fileName)
 
-    @QtCore.pyqtSlot()
-    def saveAsNoteSlot():
-        pass
+            # Save text file
+            if fileName and extension == '.txt':
+                self.centralWidget.saveFile(fileName)
+                self.currentFile = fileName
+                self.statusBar().showMessage('File saved.')
+                self.needsSave = False
+                self.window_title = f'Notepad App - {os.path.basename(fileName)}'
+                self.setWindowTitle(self.window_title)
+                return True
 
-class Ui(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(Ui, self).__init__()
-        uic.loadUi('app.ui', self)
-        self.show()
+            # Save PDF
+            elif fileName and extension == '.pdf':
+                self.savePDF(fileName)
+                return True
+
+            # File dialog canceled
+            return False
+
+        else:
+            self.centralWidget.saveFile(self.currentFile)
+            self.statusBar().showMessage('File saved.')
+            self.needsSave = False
+            return True
+
+    def saveAsNoteSlot(self):
+        
+        oldFile = self.currentFile
+        self.currentFile = ''
+
+        # File dialog canceled, reset to original file name
+        if self.saveEvent():
+            self.currentFile = oldFile
+            return False
+
+        return True
+
+
 
 if __name__ == '__main__':
     import sys
